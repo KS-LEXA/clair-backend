76088aaca447dbb50770c26c3a86efb715c68558
Delay's Mac <jy993366@gmail.com>
2026-05-20 13:30:17 +0900

    feat: 채팅 세션 관리 API 예외 처리 및 DB 스키마 보완

--- app/main.py
+++ app/main.py
@@ -22,8 +22,8 @@ app = FastAPI(title=settings.app_name, docs_url="/docs", redoc_url="/redoc", lif
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.cors_origins_list,
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
